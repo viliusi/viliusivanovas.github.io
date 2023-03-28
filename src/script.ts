18241cdf107da695359d2d50d1d1e4f6c402b778
Vilius Ivanovas <ivanovasvilius@gmail.com>
2023-03-28 18:40:31 +0200

    From nothing, to working typescript, fonts and a better understanding

--- src/script.ts
+++
@@ -1,7 +0,0 @@
-function sayHelloWorld(): void {
-    console.log("\n");
-    console.log("*** Hello World from Type Script code. ***");
-    console.log("\n");
-  }
-  
-  sayHelloWorld();
---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "John Programming";
+document.body.textContent = greeter(user);
